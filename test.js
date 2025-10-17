b2ed9084e69905db31c01ca73d8c28f5b37cd2ab
aaafasf <csc.calle@yavirac.edu.ec>
2025-10-16 21:38:44 -0500

    Corrección: reemplazo de ProcessingInstruction por process.exit

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-const sumar= require('./index');
+const sumar = require('./index');
 
-if (sumar(2,3)===5){
-    console.log ("Test pasado");
-    ProcessingInstruction.exit(0);
-    } else {
-        console.log("Test fallido");
-        process.exit(1);
-} 
+if (sumar(2, 3) === 5) {
+    console.log("Test pasado");
+    process.exit(0);  // Cambié ProcessingInstruction.exit(0) por process.exit(0)
+} else {
+    console.log("Test fallido");
+    process.exit(1);  // Esto terminará el proceso con el código de error 1 si el test falla
+}
